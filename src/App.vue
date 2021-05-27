eaccb462cad83b2ad18bcdf17bdbdffff69d1fde
manghuy <wlwhsny@naver.com>
2021-05-28 01:11:30 +0900

    git commit

--- src/App.vue
+++ src/App.vue
@@ -6,3 +6,14 @@
 export default {
 }
 </script>
+<style>
+
+
+@font-face {
+    font-family: 'KakaoOTFRegular';
+    src: url('/src/assets/KakaoOTFRegular') format(Example: 'truetype' or 'opentype' depending on the file extension of your font);
+}
+body {
+    font-family: 'KakaoOTFRegular', sans-serif;
+}
+</style>
